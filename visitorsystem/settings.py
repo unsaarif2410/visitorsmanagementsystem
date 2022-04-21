bfe8c6b792d95ea1e08e7d2c0144ee8aba6b56c4
unsaarif2410 <57376539+unsaarif2410@users.noreply.github.com>
2022-04-22 04:59:15 +0500

    Update settings.py

--- visitorsystem/settings.py
+++ visitorsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tf4jrbyxatuo_f)7pxdkcu6nnxb+n3irh2$*g6$02es1vle57i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','pcsirvms.herokuapp.com']
 
 # Application definition
 
